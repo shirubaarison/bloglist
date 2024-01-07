4f6a0504a3762223d17d4af22f1824872ea698f5
Alysson Silva <shirubaarison@gmail.com>
2024-01-07 18:22:20 -0300

    helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
+const dummy = () => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 1) return blogs[0]['likes']
+    else if (blogs.length === 0) return 0
+    else {
+        return blogs.reduce((sum, o) => sum + o['likes'], 0)
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
