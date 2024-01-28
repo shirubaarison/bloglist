a6517537d97c005771781eaf1147a6bf97849aae
Alysson Silva <shirubaarison@gmail.com>
2024-01-28 11:09:13 -0300

    bloglist expansion, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.startsWith('Bearer ')) {
+		return authorization.replace('Bearer ', '')
+	}
+
+	return null
+}
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog
@@ -15,7 +25,12 @@ blogsRouter.post('/', async (request, response) => {
 		return response.status(400).end()
 	}
 
-	const user = await User.findById(body.userId)
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: 'token invalid' })
+	}
+
+	const user = await User.findById(decodedToken.id)
 
 	const blog = new Blog({
 		title: body.title,
--- utils/middleware.js
+++ utils/middleware.js
@@ -7,6 +7,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'ValidationError') {
         return response.status(400).send({ error: 'ValidationError' })
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({ error: error.message })
     }
 
     next(error)
